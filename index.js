c13235f58581476f4ff0b22aac572b1bb8b94dae
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-09 10:49:14 -0300

    install express

--- index.js
+++ index.js
@@ -5,7 +5,32 @@ let server = http.createServer((req,res) => {
 console.log('URL:',req.url);
 console.log('METHOD:',req.method);
 
-res.end('ok');
+switch(req.url){
+
+    case '/':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/html');
+        res.end('<h1>Olá</h1>');
+
+    break;
+
+    case '/users':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify({
+
+            users:[{
+                name:'hugo',
+                email:'hugo@gmail.com',
+                id: 1
+            }]
+        }));
+
+    break;
+
+}
 
 });
 
