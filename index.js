f3cffc25d506f03fa44e2c3ce5ff41a00b6a57fa
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-09 11:25:16 -0300

    separating the routes

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 const express = require('express');
-
+let routesIndex = require('./routes/index.js')
+let routesUsers = require('./routes/users.js')
 
 let app = express();
 
-app.get('/', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Olá</h1>');
-
-});
-
-app.get('/users', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-
-        users: [{
-            name: 'hugo',
-            email: 'hugo@gmail.com',
-            id: 1
-        }]
-    });
-});
+app.use(routesIndex);
+app.use('/users',routesUsers);
 
 app.listen(3000, 'localhost', () => {
 
     console.log('servidor rodando!');
 
 });
+
