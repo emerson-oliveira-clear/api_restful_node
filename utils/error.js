f0b3879a7a0591622aa3e53d347193d7d7e79642
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-10 09:35:11 -0300

    refactoring code

--- utils/error.js
+++ utils/error.js
@@ -1,11 +1,9 @@
-/*module.exports = {
+module.exports = {
     send: (err, req, res, code = 400) => {
 
         console.log(`error: ${err}`)
         res.status(code).json({
             error: err
         })
-        
-
     }
-};*/
+};
